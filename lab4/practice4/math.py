ca8512febd6a3a76dcd78d52ec0777ed0f7e1e0e
dmitriy <kuttybaevdmitriy@gmail.com>
2026-02-27 17:32:45 +0500

    UPD 4 PRACTICE

---
+++ lab4/practice4/math.py
@@ -0,0 +1,37 @@
+#Write a Python program to convert degree to radian.
+import math
+
+degree = float(input("Input degree: "))
+radian = degree * (math.pi / 180)
+
+print("Output radian:", round(radian, 6))
+
+
+#Write a Python program to calculate the area of a trapezoid.
+height = float(input("Height: "))
+base1 = float(input("Base, first value: "))
+base2 = float(input("Base, second value: "))
+
+area = 0.5 * (base1 + base2) * height
+
+print("Expected Output:", area)
+    
+
+#Write a Python program to calculate the area of regular polygon.
+import math
+
+n = int(input("Input number of sides: "))
+s = float(input("Input the length of a side: "))
+
+area = (n * s * s) / (4 * math.tan(math.pi / n))
+
+print("The area of the polygon is:", round(area, 0))
+
+ 
+#Write a Python program to calculate the area of a parallelogram.
+base = float(input("Length of base: "))
+height = float(input("Height of parallelogram: "))
+
+area = base * height
+
+print("Expected Output:", area)
