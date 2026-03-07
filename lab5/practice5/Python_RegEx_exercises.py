c1bd4db240752ddc6b8353a4bfe39d8dab348aa4
dmitriy <kuttybaevdmitriy@gmail.com>
2026-03-07 10:49:29 +0500

    upd

--- lab5/practice5/Python_RegEx_exercises.py
+++ lab5/practice5/Python_RegEx_exercises.py
@@ -1,42 +1,41 @@
 import re
 
-
-# 1 ex
+# 1 
 s = input()
 print(bool(re.fullmatch(r"ab*", s)))
 
-# 2 ex
+# 2 
 s = input()
 print(bool(re.fullmatch(r"ab{2,3}", s)))
 
-# 3 ex
+# 3 
 s = input()
 print(re.findall(r"[a-z]+_[a-z]+", s))
 
-# 4 ex
+# 4 
 s = input()
 print(re.findall(r"[A-Z][a-z]+", s))
 
-# 5 ex
+# 5 
 s = input()
 print(bool(re.fullmatch(r"a.*b$", s)))
 
-# 6 ex
+# 6 
 s = input()
 print(re.sub(r"[ ,\.]", ":", s))
 
-# 7 ex
+# 7 
 s = input()
 print(re.sub(r"_([a-z])", lambda x: x.group(1).upper(), s))
 
-# 8 ex
+# 8 
 s = input()
 print(re.split(r"(?=[A-Z])", s))
 
-# 9 ex
+# 9 
 s = input()
 print(re.sub(r"(?<!^)(?=[A-Z])", " ", s))
 
-# 10 ex
+# 10 
 s = input()
 print(re.sub(r"([A-Z])", r"_\1", s).lower().lstrip("_"))
